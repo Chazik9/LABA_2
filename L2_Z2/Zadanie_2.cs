986967e57dd6946559f1970d1988edb97987bc41
Chazik9 <118742760+Chazik9@users.noreply.github.com>
2024-10-17 22:33:02 +0500

    Update Zadanie_2.cs

--- L2_Z2/Zadanie_2.cs
+++ L2_Z2/Zadanie_2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -13,11 +13,15 @@ namespace L2_Z2
     {
         static void Main(string[] args)
         {
+            int maxiZnach= 2147483647;
+            int miniZnach = -247483648;
+            bool isNormalDiapozon=true;
+            bool emptySequnce = false;
             int elementOfSequence = 0;//Элементы послед-ти
             int itemCounter = 1;//Проверка номеров элементов 
-            int elementCounter=1;//Счётчик номеров элементов
+            int elementCounter = 1;//Счётчик номеров элементов
             bool isNumber = false;//Отвечает за проверку на целые числа элементов послед-ти 
-            int summOfNumver=0;//сумма элементов послед-ти стоящих н нечёт местах
+            int summOfNumver = 0;//сумма элементов послед-ти стоящих н нечёт местах
             Console.WriteLine("Я программа, которая находит сумму элементов с нечетными номерами последовательности.");
             while (true)//Пока элемент не равен 0
             {
@@ -30,12 +34,23 @@ namespace L2_Z2
                     {
                         Console.WriteLine("Введено неправильное значение, попробуйте ещё раз");
                     }
-                } while (!isNumber); // проверка на не целое число
+                    if (itemCounter % 2 == 1 && elementOfSequence + summOfNumver > maxiZnach)
+                    {
+                        isNormalDiapozon = false;
+                        Console.WriteLine("Введите число поменьше, так как сумма будет слишком большая");
+                    }
+                    if (itemCounter % 2 == 1 && elementOfSequence + summOfNumver < miniZnach)
+                    {
+                        isNormalDiapozon = false;
+                        Console.WriteLine("Введите число побольше, так как сумма будет слишком маленькая");
+                    }
+                } while (!isNumber || !isNormalDiapozon); // проверка на не целое число
                 if (elementOfSequence == 0)//проверка на конец послед-ти
                 {
                     if (elementCounter == 1)//проверка на пустую послед-ть
                     {
-                        Console.Write("Последовательность пустая => ");
+                        emptySequnce = true;
+                        Console.WriteLine("Последовательность пустая");
                     }
                     break;
                 }
@@ -48,7 +63,10 @@ namespace L2_Z2
 
 
             }
-            Console.WriteLine($"Сумма элементов с нечётными номерами равна {summOfNumver}");
+            if (!emptySequnce)
+            {
+                Console.WriteLine($"Сумма элементов с нечётными номерами равна {summOfNumver}");
+            }
         }   
     }
 }
